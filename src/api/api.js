cd22a4acb6dce65b32472a74f8e48ed25e0f0e3d
rohit202k3 <rohit2k3pandey@gmail.com>
2026-04-22 15:05:07 +0530

    fix base url https

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "stunning-serenity-production-d6da.up.railway.app/api",
+  baseURL: "https://stunning-serenity-production-d6da.up.railway.app/api",
 });
 
 // 🔥 ADD THIS (VERY IMPORTANT)

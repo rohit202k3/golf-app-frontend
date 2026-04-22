c1e36b249ab7053f29aca72b112a9845bf797b26
rohit202k3 <rohit2k3pandey@gmail.com>
2026-04-22 14:49:31 +0530

    updated api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "stunning-serenity-production-d6da.up.railway.app",
 });
 
 // 🔥 ADD THIS (VERY IMPORTANT)
